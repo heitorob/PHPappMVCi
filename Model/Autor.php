cc48a6cc3223206b2c9ee166a48a8c10b68ecad2
231009 <231009@CPD03-M07>
2025-05-12 10:52:08 -0300

    Criação dos models de Categoria, Autor e inserção do model Usuário.

---
+++ Model/Autor.php
@@ -0,0 +1,70 @@
+<?php
+    namespace PHPappMVCi\Model;
+
+    use PHPappMVCi\DAO\AutorDAO;
+    use Exception;
+
+    final class Autor extends Model
+    {
+        public ?int $Id = null;
+
+        public ?string $Nome
+        {
+            set
+            {
+                if(strlen($value) < 4)
+                    throw new Exception("Nome deve ter no mínimo 4 caracteres.");
+
+                    $this->Nome = $value;
+            }
+
+            get => $this->Nome ?? null;
+        }
+
+        public ?string $Nascimento
+        {
+            set
+            {
+                if(empty($value))
+                    throw new Exception("Preencha a data de nascimento.");
+
+                    $this->Nascimento ?? null;
+            }
+
+            get => $this->Nascimento ?? null;
+        }
+
+        public ?string $CPF
+        {
+            set
+            {
+                if(strlen($value) < 11)
+                    throw new Exception("Preencha o CPF corretamente.");
+
+                    $this->CPF = $value;
+            }
+
+            get => $this->CPF ?? null;
+        }
+
+        function save() : Autor
+        {
+            return new AutorDAO()->save($this);
+        }
+
+        function getById(int $id) : ?Autor
+        {
+            return new AutorDAO()->selectById($this);
+        }
+
+        function getAllRows() : array
+        {
+            $this->rows = new AutorDAO()->select();
+        }
+
+        function delete(int $id) : bool
+        {
+            return new AutorDAO()->delete($id);
+        }
+    }
+?>
---
+++ Model/Categoria.php
@@ -0,0 +1,44 @@
+<?php
+    namespace PHPappMVCi\Model;
+
+    use PHPappMVCi\DAO\CategoriaDAO;
+    use Exception;
+
+    final class Categoria extends Model
+    {
+        public ?int $Id = null;
+
+        public ?string $Descricao
+        {
+            set
+            {
+                if(strlen($value) < 4)
+                    throw new Exception("Descricao deve ter no mínimo 4 caracteres.");
+
+                    $this->Descricao = $value;
+            }
+
+            get => $this->Descricao ?? null;
+        }
+
+        function save() : Categoria
+        {
+            return new CategoriaDAO()->save($this);
+        }
+
+        function getById(int $id) : ?Categoria
+        {
+            return new CategoriaDAO()->selectById($this);
+        }
+
+        function getAllRows() : array
+        {
+            $this->rows = new CategoriaDAO()->select();
+        }
+
+        function delete(int $id) : bool
+        {
+            return new CategoriaDAO()->delete($id);
+        }
+    }
+?>
